f0bdc841d0220b598167f269920fe3721b590523
yic242 <yic242@eng.ucsd.edu>
2017-08-21 00:14:32 -0700

    Update non_alcoholic_data.py

--- non_alcoholic_data.py
+++ non_alcoholic_data.py
@@ -5,9 +5,9 @@ def parseData(fname):
         yield eval(l)
 
 if __name__ == "__main__":
-  print "Reading Data..."
+  print("Reading Data...")
   data = list(parseData("http://jmcauley.ucsd.edu/cse255/data/beer/non-alcoholic-beer.json"))
-  print "done"
+  print("done")
 
 
   #comment
